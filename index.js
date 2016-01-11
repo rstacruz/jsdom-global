2c10f817d9697f3b9e42fdec7f761782a12fb176
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-11 11:30:46 +0800

    Rearrange

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
  * enables jsdom globally.
  */
 
+var html = '<!doctype html><html><head><meta charset="utf-8">' +
+  '</head><body></body></html>'
+
+var blacklist = [
+  'ArrayBuffer', 'Int8Array', 'Uint8Array', 'Uint8ClampedArray',
+  'Int16Array', 'Uint16Array', 'Int32Array', 'Uint32Array', 'Float32Array',
+  'Float64Array', 'toString', 'constructor', 'console', 'setTimeout',
+  'clearTimeout', 'setInterval', 'clearInterval' ]
+
 module.exports = function globalJsdom (func) {
   if (typeof func === 'function') {
     try {
@@ -17,23 +26,14 @@ module.exports = function globalJsdom (func) {
 
 function globalize () {
   var jsdom = require('jsdom')
-
-  var html = '<!doctype html><html><head><meta charset="utf-8"></head><body></body></html>'
-  var blacklist = [
-    'ArrayBuffer', 'Int8Array', 'Uint8Array', 'Uint8ClampedArray',
-    'Int16Array', 'Uint16Array', 'Int32Array', 'Uint32Array', 'Float32Array',
-    'Float64Array', 'toString', 'constructor', 'console', 'setTimeout',
-    'clearTimeout', 'setInterval', 'clearInterval' ]
-
   var document = jsdom.jsdom(html)
   var window = document.defaultView
   var keys = []
 
   Object.keys(window).forEach((key) => {
-    if (blacklist.indexOf(key) === -1) {
-      keys.push(key)
-      global[key] = window[key]
-    }
+    if (blacklist.indexOf(key) !== -1) return
+    keys.push(key)
+    global[key] = window[key]
   })
 
   global.document = document
